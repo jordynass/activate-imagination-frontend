24dbe997f8300082f5d2542e512fe81ced60ad2c
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2025-01-07 12:18:52 -0500

    Implement basic camera integration
    Takes and displays a picture from the camera
    -uses low quality image for performance
    -shows image
    -gets base64 string of image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,47 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { useRef, useState } from 'react';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { Button, StyleSheet, Image, Text, View } from 'react-native';
 
 export default function TabOneScreen() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const cameraRef = useRef<CameraView | null>(null);
+  const [photoUri, setPhotoUri] = useState<string | null>(null);
+  const [photoBase64, setPhotoBase64] = useState<string | null>(null);
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  async function takePhoto() {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync({
+        base64: true,
+        quality: 0.2,
+      });
+      if (photo?.base64) {
+        setPhotoBase64(photo.base64);
+        setPhotoUri(photo.uri);
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <CameraView ref={cameraRef} style={styles.camera}>
+        <View style={styles.buttonContainer}>
+          <Button title="Take Photo" onPress={takePhoto} />
+        </View>
+      </CameraView>
+      {photoUri && <Image source={{ uri: photoUri }} style={styles.photo} />}
     </View>
   );
 }
@@ -19,13 +52,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  camera: {
+    width: 300,
+    height: 300,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  message: {
+    fontSize: 16,
+  },
+  photo: {
+    width: 300,
+    height: 300,
+    marginTop: 20,
   },
 });
