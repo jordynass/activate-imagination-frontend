cf8a31af2c9d8b7acf5a76eac3764d7dd1b48319
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2025-01-13 20:01:29 -0500

    Connect to Cloud Run backend instance

--- utils/network.ts
+++ utils/network.ts
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 export function getWebSocketUrl() {
   // TODO: Move to a .env variable using expo-constants
-  // and include non-local environments.
-  return "ws://192.168.1.194:3000";
+  // and include local environment.
+  return "https://activate-imagination-backend-605301331241.us-central1.run.app";
 }
 
 const socket = io(getWebSocketUrl());
